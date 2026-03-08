578a0e24656e0b918502b3b4b7da6031c5b33747
wirbel <womp@herr-der-mails.de>
2026-03-08 12:49:57 +0100

    cosmetics

--- ET4000v2_pins.h
+++ ET4000v2_pins.h
@@ -17,73 +17,74 @@
  ******************************************************************************/
 
 /* X axis, checked. */
-#define XSTEP   /* PD7 */ 15 /* checked.   */
-#define XDIR    /* PC5 */ 21 /* checked.   */
-#define XENABLE /* PD6 */ 14 /* checked, active low */
+// NOTE: XENABLE might be shared w. other axis, tbd. 
+#define XSTEP                           15 // PD7, checked.
+#define XDIR                            21 // PC5, checked.
+#define XENABLE                         14 // PD6, checked, active low
 
 /* Y axis */
-// #define YSTEP   /* PC4 */ 20 /* WRONG - collides with end switches             */
-// #define YDIR    /* PC3 */ 19 /* WRONG - collides with end switches             */
-// #define YENABLE /* PC2 */ 18 /* WRONG - collides with end switches, active low */
+// #define YSTEP                        20 // PC4, WRONG - collides with end switches
+// #define YDIR                         19 // PC3, WRONG - collides with end switches
+// #define YENABLE                      18 // PC2, WRONG - collides with end switches, active low
 
 /* Z axis */
-// #define ZSTEP   /* PC1 */ 17 /* collides with J1_SCL             */
-// #define ZDIR    /* PC0 */ 16 /* collides with J1_SDA             */
-// #define ZENABLE /* PD2 */ 10 /* collides with J1_RX1, active low */
+// #define ZSTEP                        17 // PC1, collides with J1_SCL
+// #define ZDIR                         16 // PC0, collides with J1_SDA
+// #define ZENABLE                      10 // PD2, collides with J1_RX1, active low
 
 /* Port E, extruder */
-// #define ESTEP   /* PD5 */ 13 /* WRONG, this is BEDHEATER */
-// #define EDIR    /* PD4 */ 12 /* WRONG, this is HOTEND */
-// #define EENABLE /* PD3 */ 11 /* collides with J1_TX1, active low */
+// #define ESTEP                        13 // PD5, WRONG, this is BEDHEATER
+// #define EDIR                         12 // PD4, WRONG, this is HOTEND
+// #define EENABLE                      11 // PD3, collides with J1_TX1, active low
 
 /* End switches, in fw as min pos switches.
  * 1. VCC
  * 2. GND
  * 3. contact
  */
-#define XMIN    /* PC2 */ 18 /* checked. */
-#define YMIN    /* PC3 */ 19 /* checked. */
-#define ZMIN    /* PC4 */ 20 /* checked. */
+#define XMIN                            18 // PC2, checked.
+#define YMIN                            19 // PC3, checked.
+#define ZMIN                            20 // PC4, checked.
 
 
 /* The MOSFETs switch to GND (Low-Side Switch):
  *   pinMode(FAN, OUTPUT);
  *   digitalWrite(FAN, HIGH); // turns fan on (and it's LED)
  */
-#define BEDHEATER /* PD5 */ 13 /* PWM: yes, checked. */
-#define HOTEND    /* PD4 */ 12 /* PWM: yes, checked. */
-#define FAN       /* PB4 */  4 /* PWM: yes, checked. */
-#define LED       /* PB4 */  4 /* connected to FAN, checked. */
+#define BEDHEATER                       13 // PD5, PWM: yes, checked.
+#define HOTEND                          12 // PD4, PWM: yes, checked.
+#define FAN                             4  // PB4, PWM: yes, checked.
+#define LED                             4  // PB4, connected to FAN, checked.
 
-#define TBED      /* PA6 */ A6 /* PWM: no, checked. */
-#define THOTEND   /* PA7 */ A7 /* PWM: no, checked. */
+#define TBED                            A6 // PWM PA6: no, checked.
+#define THOTEND                         A7 // PWM PA7: no, checked.
 
 /* UART && I2C/Wire:
- *   NOTE: CH340 DTR/RTS is connected to the RESET pin of
- *         the ATmega1284 via 100nF-cap.
+ *   NOTE: CH340 DTR/RTS is connected to the RESET pin of the uC via 100nF.
  */
-#define SERIAL_RX  /* PD0 */ 8  /* via USB, checked. */
-#define SERIAL_TX  /* PD1 */ 9  /* via USB, checked. */
-#define SERIAL1_RX /* PD2 */ 10 /* on J1, checked.   */
-#define SERIAL1_TX /* PD3 */ 11 /* on J1, checked.   */
-#define I2C_SCL    /* PC0 */ 16 /* checked.          */
-#define I2C_SDA    /* PC1 */ 17 /* checked.          */
-
-
-
+#define SERIAL_RX                       8  // PD0, UART0 via USB, checked.
+#define SERIAL_TX                       9  // PD1, UART0 via USB, checked.
+#define SERIAL1_RX                      10 // PD2, UART1 on J1, checked.
+#define SERIAL1_TX                      11 // PD3, UART1 on J1, checked.
+#define I2C_SCL                         16 // PC0, I2C on J1, checked.
+#define I2C_SDA                         17 // PC1, I2C on J1, checked.
 
 /***** J1: 12-pol LCD pin header ****
  * NOTE: J1 Pin1 is marked as square pad.
- *                     12 VCC (+5V)*
- *                     11 GND      */
-#define J1_MOSI 5   /* 10 MOSI PB5, checked. */
-#define J1_SCK  7   /*  9 SCK  PB7, checked. */
-#define J1_RX1  10  /*  8 RX1  PD2, checked. */
-#define J1_A4   A4  /*  7 A4   PA4, checked. */
-#define J1_TX1  11  /*  6 TX1  PD3, checked. */
-#define J1_A3   A3  /*  5 A3   PA3, checked. */
-#define J1_SCL  16  /*  4 SCL  PC0, checked. */
-#define J1_A2   A2  /*  3 A2   PA0, checked. */
-#define J1_SDA  17  /*  2 SDA  PC1, checked. */
-#define J1_A1   A1  /*  1 A1   PA1, checked. */
+ */
+//                                          //      J1/12, +5V
+//                                          //      J1/11, GND
+#define J1_MOSI                         5   // PB5, J1/10, MOSI, checked.
+#define J1_SCK                          7   // PB7, J1/9 , SCK , checked.
+#define J1_RX1                          10  // PD2, J1/8 , RX1 , checked.
+#define J1_A4                           A4  // PA4, J1/7 , A4  , checked.
+#define J1_TX1                          11  // PD3, J1/6 , TX1 , checked.
+#define J1_A3                           A3  // PA3, J1/5 , A3  , checked.
+#define J1_SCL                          16  // PC0, J1/4 , SCL , checked.
+#define J1_A2                           A2  // PA0, J1/3 , A2  , checked.
+#define J1_SDA                          17  // PC1, J1/2 , SDA , checked.
+#define J1_A1                           A1  // PA1, J1/1 , A1  , checked.
+
+/***** SDCARD ****/
+// not yet known.
 
